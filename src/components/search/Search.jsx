e283fec0c14d9fac48d2be1cec88ebe4bd08b6a8
André de Souza <andre_aru010@hotmail.com>
2023-09-02 16:08:12 -0300

    Update Search.jsx

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../moviecard/MovieCard";
 
-import { AiFillStar } from "react-icons/ai";
-
 const searchUrl = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
 
